59904c1bcc3aefd8bd3024713a10039dcefb396c
210076331 <james.damato@ge.com>
2017-07-24 16:27:19 -0400

    solution to two-sum

--- algorithms/two_sum/solution.py
+++ algorithms/two_sum/solution.py
@@ -5,4 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        for ind, val in enumerate(nums):
+            for ind2, val2 in enumerate(nums):
+                if val + val2 == target and ind != ind2:
+                    return (ind, ind2)
+        raise ValueError('No two sums exist!')
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.twoSum([1, 2, 3, 4, 5], 3)
